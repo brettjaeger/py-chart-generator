25eebf6eada2446ece6886ebe7ea40831ff322e3
Brett Jaeger <brettjaeger@gmail.com>
2023-12-10 19:21:45 -0800

    Added the ability to copy only two columns. Adjusted the readme to match.

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ A script to make charts easily from google sheets
 
 ## Instructions
 
-1. Arrange your data so it can be copy/pasted into three columns
+1. Arrange your data so it can be copied to the clipboard in two (or three) columns.
 2. The columns are:
     1. X-Axis
     2. Y-Axis
-    3. Series
+    3. Series (optional)
 3. Run chart_generator.py
 4. Select "s" for scatter plot or "l" for line plot
 5. The graph will be generated in a web browser!
--- chart_generator.py
+++ chart_generator.py
@@ -5,14 +5,22 @@ input("Please collect your columns in the clipboard")
 graph_type = input("Graph type: Scatter=s Line=l")
 
 data = pd.read_clipboard(sep='\t')
-
-if graph_type == 's':
-    fig = px.scatter(data, x=data.columns[0], y=data.columns[1], color=data.columns[2], symbol=data.columns[2])
-    fig.show()
-elif graph_type == 'l':
-    fig = px.line(data, x=data.columns[0], y=data.columns[1], color=data.columns[2], symbol=data.columns[2])
-    fig.show()
-else:
-    print("Graph type error")
-
+try:
+    if graph_type == 's':
+        fig = px.scatter(data, x=data.columns[0], y=data.columns[1], color=data.columns[2], symbol=data.columns[2])
+        fig.show()
+    elif graph_type == 'l':
+        fig = px.line(data, x=data.columns[0], y=data.columns[1], color=data.columns[2], symbol=data.columns[2])
+        fig.show()
+    else:
+        print("Graph type error")
+except IndexError:
+    if graph_type == 's':
+        fig = px.scatter(data, x=data.columns[0], y=data.columns[1])
+        fig.show()
+    elif graph_type == 'l':
+        fig = px.line(data, x=data.columns[0], y=data.columns[1])
+        fig.show()
+    else:
+        print("Graph type error")    
 
