c06453607d3e74831387abda1647f96940248934
Brett Jaeger <brettjaeger@gmail.com>
2023-12-30 03:56:53 -0800

    Added a feature to include the origin on graphs by default. Updated the readme and the .exe file as well

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ A script to make charts easily from google sheets in Windows
 3. Run chart_generator.exe (windows), or chart_generator.py (linux)
 4. A command prompt window will open and ask you to make sure your data is added to the clipboard
 5. Select "s" for scatter plot or "l" for line plot
-6. The graph will be generated in a web browser! The graph can then be saved as a PNG
+6. Next, you will be prompted to include the origin or not. Press "y" for yes or "n" if you want the axes to be auto-scaled. The default response is "y."
+7. The graph will be generated in a web browser and can then be saved as a PNG.
 
 ## Example Data
 
--- chart_generator.py
+++ chart_generator.py
@@ -2,25 +2,31 @@ import plotly.express as px
 import pandas as pd
 
 input("Please collect your columns in the clipboard")
-graph_type = input("Graph type: Scatter=s Line=l [s,l] ")
 
-data = pd.read_clipboard(sep='\t')
+graph_type = input("Graph type: Scatter=s Line=l [s,l] ")
+include_origin = input("Do you want the graph to include the origin? [Y/n]") or 'y'
+
+data = pd.read_clipboard(sep='\t') #create dataset in pandas
 try:
     if graph_type == 's':
         fig = px.scatter(data, x=data.columns[0], y=data.columns[1], color=data.columns[2], symbol=data.columns[2])
-        fig.show()
     elif graph_type == 'l':
         fig = px.line(data, x=data.columns[0], y=data.columns[1], color=data.columns[2], symbol=data.columns[2])
-        fig.show()
     else:
         print("Graph type error")
+    # Set up axes to include origin
+    if include_origin == 'y':
+        fig.update_xaxes(rangemode='tozero')
+    fig.show()
 except IndexError:
     if graph_type == 's':
         fig = px.scatter(data, x=data.columns[0], y=data.columns[1])
-        fig.show()
     elif graph_type == 'l':
         fig = px.line(data, x=data.columns[0], y=data.columns[1])
-        fig.show()
     else:
-        print("Graph type error")    
+        print("Graph type error")   
+    # Set up axes to include origin
+    if include_origin == 'y':
+        fig.update_xaxes(rangemode='tozero')
+    fig.show() 
 
